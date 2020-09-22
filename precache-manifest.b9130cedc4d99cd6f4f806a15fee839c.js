25e792cb8a7aa14273261d5b9d088f215b47b446
Rauf Ismayilov <rauf.raymond110@gmail.com>
2020-09-22 20:57:11 +0400

    Updates

---
+++ precache-manifest.b9130cedc4d99cd6f4f806a15fee839c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f724f60a74adf2a5698c3bb8d36447b",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "e5c458d4d6c4c110bd70",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a98b18484ac687725bf",
+    "url": "/deploy-test/static/js/2.d8428b5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.d8428b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c458d4d6c4c110bd70",
+    "url": "/deploy-test/static/js/main.9dd6dfe3.chunk.js"
+  },
+  {
+    "revision": "8b5a7099889fc3b8cc02",
+    "url": "/deploy-test/static/js/runtime-main.6b56c7a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
